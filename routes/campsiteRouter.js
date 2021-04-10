798736aa45fe1ffc1571b68bed3750c69a783873
naciom9 <naciom9@yahoo.com>
2021-04-10 12:56:34 -0500

    Added partners and promotions routes

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,25 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send you a campsite ${req.params.campsiteId}`);
+})
+.post((req, res) => {
+    res.end(`Will add details to campsite: ${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
